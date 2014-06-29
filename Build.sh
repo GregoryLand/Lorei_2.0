330644bc282f3b6505a1e16e16e728907d26c84e
GregoryLand <landjgregory@gmail.com>
2014-06-29 12:47:28 -0400

    Added some scripts to build things for us

---
+++ Build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd BuildScripts/Linux
+./Build.sh
--- BuildScripts/Linux/Build.sh
+++ BuildScripts/Linux/Build.sh
@@ -3,14 +3,18 @@
 x86ReleaseDir=Build/Linux/Release/x86
 x64ReleaseDir=Build/Linux/Release/x64
 x86DebugDir=Build/Linux/Debug/x86
-x64ReleaseDir=Build/Linux/Debug/x64
+x64DebugDir=Build/Linux/Debug/x64
 
 cd ..
 cd ..
+
+projectDir=$(pwd)
 
 mkdir -p Build/Linux/Release/x86
 mkdir -p Build/Linux/Release/x64
 mkdir -p Build/Linux/Debug/x86
 mkdir -p Build/Linux/Debug/x64
 
-
+cd Build/Linux/Debug/x64
+cmake ../../../../
+make
