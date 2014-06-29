a33d75cfca89b77f40e87c1a9eb4d2101e938338
GregoryLand <landjgregory@gmail.com>
2014-06-29 12:09:07 -0400

    Creating basic build script for linux.  Only makes dir structure currently

---
+++ BuildScripts/Linux/Build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+x86ReleaseDir=Build/Linux/Release/x86
+x64ReleaseDir=Build/Linux/Release/x64
+x86DebugDir=Build/Linux/Debug/x86
+x64ReleaseDir=Build/Linux/Debug/x64
+
+cd ..
+cd ..
+
+mkdir -p Build/Linux/Release/x86
+mkdir -p Build/Linux/Release/x64
+mkdir -p Build/Linux/Debug/x86
+mkdir -p Build/Linux/Debug/x64
+
+
